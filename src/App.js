449240fa3fb1e4bf29a4992b86348c16afbc964a
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-05-04 16:55:52 +0100

    New version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="weather" className="weather-container">
+        <br />
+        <div id="footer">
+          Created by Alyssa Reyes {""}
+          <a
+            href="https://github.com/Lyching0310/React-Weather-App"
+            target="_blank"
+            rel="noreferrer"
+            id="gitLink"
+          >
+            Open-source code
+          </a>
+          {""} and {""}
+          <a
+            href="https://alyssareactweatherapp.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            id="Netlify"
+          >
+            Hosted on Netlify
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
